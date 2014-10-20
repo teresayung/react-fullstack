93c1bb4eb3e88b487dc7fcd1dae81facffad969a
Marc Wilhite <marc.wilhite@gmail.com>
2014-10-20 15:50:49 -0700

    first commit

---
+++ templates/app/src/components/Banner.jsx
@@ -0,0 +1,15 @@
+var React = require('react');
+
+var BANNER = React.createClass({
+ 
+  
+  render: function(){
+    return (
+      <div className="container">
+        <img src="./image/slush.png"/>
+      </div>
+    );
+  }
+})
+
+module.exports = BANNER;
---
+++ templates/app/src/components/NavBar.jsx
@@ -0,0 +1,50 @@
+'use strict';
+
+var React = require('react');
+
+var AppStore = require('../stores/AppStore');
+var AppActions = require('../actions/AppActions');
+
+function getAppState(){
+  return AppStore.getData()
+};
+
+var NAV = React.createClass({
+  getInitialState: function(){
+    return getAppState();
+  },
+
+  _onChange: function(){
+    this.setState(getAppState());
+  },
+
+  componentDidMount: function(){
+    AppStore.addChangeListener(this._onChange);
+  },
+
+  componentWillUnmount: function(){
+    AppStore.removeChangeListener(this._onChange);
+  },
+
+  handleClick: function(){
+    AppActions.exampleAction('Data from View');
+  },
+  
+  render: function(){
+    return (
+        <nav className="navbar navbar-default" role="navigation">
+          <div className="container-fluid">
+            <div className="navbar-header">
+              <a className="navbar-brand" href="#">React-Flux-Fullstack</a>
+              <ul className="nav navbar-nav navbar-right">
+                <li><a href="#">Sign up</a></li>
+                <li><a href="#">Login</a></li>
+              </ul>
+            </div>
+          </div>
+      </nav>
+    );
+  }
+})
+
+module.exports = NAV;
